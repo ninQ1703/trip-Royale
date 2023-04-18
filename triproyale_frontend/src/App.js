b2f8fbeac55f7f150451610ad61bbf0dbdf7eb3e
ninQ1703 <n_kagzi@cs.iitr.ac.in>
2023-04-18 23:34:35 +0530

    Auth integrated

--- triproyale_frontend/src/App.js
+++ triproyale_frontend/src/App.js
@@ -1,43 +1,31 @@
-import './App.css';
-import * as React from 'react';
-import PendingPayments from './mySplitPages/PendingPayments/PendingPayments';
-import AmountToBePaid from './mySplitPages/AmountToBePaid/AmountToBePaid';
-import TotalExpenses from './mySplitPages/TotalExpenses/totalExpenses';
-import Gallery from './photos_frontend/photos';
-import { BrowserRouter,Routes, Route, Navigate } from 'react-router-dom';
-import { Link
- } from 'react-router-dom';
-import SideBar from './SideBar/Sidebar';
-import NewSplit from './mySplitPages/NewSplit/NewSplits';
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import LoginReg from "./pages/auth/LoginReg";
+import ResetPassword from "./pages/auth/ResetPassword";
+import SendPasswordResetEmail from "./pages/auth/SendPasswordResetEmail";
+import Contact from "./pages/Contact";
+import Dashboard from "./DashBoard/Dashboard";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import { useSelector } from "react-redux";
 function App() {
-    const user = 1;
-    const trip = 1;
-    return (
-        <>
-        <BrowserRouter>
-        <SideBar/>
-        <div>
-            <Routes >
-                {/* <Route path='/' element={<SideBar/>}/> */}
-                {/* <Route path='/recommendations' element={}/> */}
-                <Route path='/' element={<Navigate replace to='/gallery'/>}/>
-                <Route path='/gallery' element={<Gallery user={user} trip={trip}/>}/>
-                <Route path='/expenses/myexpenses' element={<TotalExpenses user={user} trip={trip}/>}/>
-                <Route path='/expenses/amounttobepaid' element={<AmountToBePaid user={user} trip={trip}/>}/>
-                <Route path='/expenses/pendingpayments' element={ <PendingPayments user={user} trip={trip}/>}/>
-                <Route path='/newsplit' element={<NewSplit user={user} trip={trip}/>}/>
-            </Routes>
-        </div>
-            
-        </BrowserRouter>
-        {/* <NewSplit user={user} trip={trip}/> */}
-        {/* <Gallery user={user} trip={trip}/>  */}
-        {/* <TotalExpenses user={user} trip={trip}/>  */}
-        {/* <AmountToBePaid user={user} trip={trip}/> */}
-        {/* <NewSplit user={user} trip={trip}/> */}
-        </>
-    );
+  const { access_token } = useSelector(state => state.auth)
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="login" element={!access_token ? <LoginReg /> : <Navigate to="/dashboard" />} />
+            <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail />} />
+            <Route path="api/user/reset/:id/:token" element={<ResetPassword />} />
+          </Route>
+          <Route path="/dashboard" element={access_token ? <Dashboard /> : <Navigate to="/login" />} />
+          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ triproyale_frontend/src/DashBoard/EnterTripPage.js
@@ -0,0 +1,43 @@
+import './App.css';
+import * as React from 'react';
+import PendingPayments from '../mySplitPages/PendingPayments/PendingPayments';
+import AmountToBePaid from '../mySplitPages/AmountToBePaid/AmountToBePaid';
+import TotalExpenses from '../mySplitPages/TotalExpenses/totalExpenses';
+import Gallery from '../photos_frontend/photos';
+import { BrowserRouter,Routes, Route, Navigate } from 'react-router-dom';
+import { Link
+ } from 'react-router-dom';
+import SideBar from '../SideBar/Sidebar';
+import NewSplit from '../mySplitPages/NewSplit/NewSplits';
+
+function App() {
+    const user = 1;
+    const trip = 1;
+    return (
+        <>
+        <BrowserRouter>
+        <SideBar/>
+        <div>
+            <Routes >
+                {/* <Route path='/' element={<SideBar/>}/> */}
+                {/* <Route path='/recommendations' element={}/> */}
+                <Route path='/' element={<Navigate replace to='/gallery'/>}/>
+                <Route path='/gallery' element={<Gallery user={user} trip={trip}/>}/>
+                <Route path='/expenses/myexpenses' element={<TotalExpenses user={user} trip={trip}/>}/>
+                <Route path='/expenses/amounttobepaid' element={<AmountToBePaid user={user} trip={trip}/>}/>
+                <Route path='/expenses/pendingpayments' element={ <PendingPayments user={user} trip={trip}/>}/>
+                <Route path='/newsplit' element={<NewSplit user={user} trip={trip}/>}/>
+            </Routes>
+        </div>
+            
+        </BrowserRouter>
+        {/* <NewSplit user={user} trip={trip}/> */}
+        {/* <Gallery user={user} trip={trip}/>  */}
+        {/* <TotalExpenses user={user} trip={trip}/>  */}
+        {/* <AmountToBePaid user={user} trip={trip}/> */}
+        {/* <NewSplit user={user} trip={trip}/> */}
+        </>
+    );
+}
+
+export default App;
