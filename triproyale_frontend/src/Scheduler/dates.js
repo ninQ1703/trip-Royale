f8aa138c8763380b001161bae0fdd11e3e67f66a
ninQ1703 <n_kagzi@cs.iitr.ac.in>
2023-04-19 18:29:08 +0530

    buttons slecetd toggle

--- triproyale_frontend/src/Scheduler/dates.js
+++ triproyale_frontend/src/Scheduler/dates.js
@@ -8,6 +8,58 @@ import { Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap-buttons';
 import { DayPlans } from './Dayplans.js';
 import TaskAdd from './TaskAddition.js';
 import "./dates.css"
+
+
+
+const DatesButton = (props) => {
+  const [selected, setSelected] = useState("dateButton")
+
+  const changeSelect = () => {  
+    props.setprevSel(props.date)
+  }
+
+  const formatDate = (date) => {
+    let y = parseInt(date.substr(0, 4));
+    let m = parseInt(date.substring(5, 7));
+    let d = parseInt(date.substring(8, 10));
+
+    return (
+
+      <p>{d} {months[m]}</p>
+    )
+  }
+  return <Button
+    className={(props.date==props.prevSel)?"seldateButton":"dateButton"}
+    key={props.date}
+    onClick={() => {
+      props.handleClick(props.date);
+      changeSelect();
+    }}
+    aria-controls="schedule"
+    aria-expanded={props.open}
+  >
+    <div className='=' style={{ fontSize: '20px' }}>{formatDate(props.date)}
+    </div>
+  </Button>
+}
+
+
+const RenderItems = (props) => {
+  const [prevSel, setprevSel] = useState('')
+  useEffect(()=>{
+    setprevSel(props.dayplans[0])
+  }, [props.dayplans])
+
+  console.log(prevSel);
+  return (
+    <div style={{ display: 'flex', overflowX: 'auto', maxWidth: '1500px', justifyContent: 'center', }}>
+      {props.dayplans.map((date) => (
+        <DatesButton handleClick={props.handleClick} open={props.open} date={date} setprevSel={setprevSel} prevSel={prevSel}/>
+      ))}
+    </div>
+  );
+};
+
 export const Dates = (props) => {
 
   const [open, setOpen] = useState(true);
@@ -39,7 +91,7 @@ export const Dates = (props) => {
       .then((data) => setSchedule(data));
     console.log(schedule);
 
-  }, [props.user,props.trip])
+  }, [props.user, props.trip])
 
   const handleClick = async (date) => {
     const res = await fetch(`http://localhost:8000/${props.user}/${props.trip}/schedule/${date}/events`);
@@ -51,60 +103,25 @@ export const Dates = (props) => {
 
   }
 
-  const formatDate = (date) => {
-    // let m=date.getMonth()+1;
-    // let d=date.getDate();
-    // let y=date.getYear();
-    let y = parseInt(date.substr(0, 4));
-    let m = parseInt(date.substring(5, 7));
-    let d = parseInt(date.substring(8, 10));
-
-    return (
-
-      <p>{d} {months[m]}</p>
-    )
-  }
-
-  const RenderItems = () => {
-    return (
-      <div style={{ display: 'flex', overflowX: 'auto',maxWidth: '1500px',  justifyContent:'center',}}>
-        {props.dayplans.map((date) => (
-          <Button
-          className="dateButton"
-            key={date} 
-            onClick={() => {
-              handleClick(date);
-            }}
-            aria-controls="schedule"
-            aria-expanded={open}
-          >
-            <div className='=' style={{fontSize:'20px'}}>{formatDate(date)}
-            </div>
-          </Button>
-        ))}
-      </div>
-    );
-  };
-  
   console.log(schedule);
 
   return (
     <div className="text-center" style={{}}>
       <div
-        style={{ backgroundColor: "#FFE193", top:'20%',height:'9.5%', position:'fixed', width:'100%', zIndex:'1'}}
+        style={{ backgroundColor: "#FFE193", top: '20%', height: '10.5%', position: 'fixed', width: '100%', zIndex: '1' }}
       >
 
         <ButtonGroup size="lg" zIndex="2">
-          {RenderItems()}
+          <RenderItems dayplans={props.dayplans} handleClick={handleClick} open={open} />
         </ButtonGroup >
       </div>
-      
-      <div id="schedule" style={{overflowY:'initial', marginTop:'15%',zIndex:'-2'}}>
+
+      <div id="schedule" style={{ overflowY: 'initial', marginTop: '15%', zIndex: '-2' }}>
         <DayPlans Plan={schedule} DeleteTask={DeleteTask} user={props.user} trip={props.trip} />
       </div>
-      
-      <TaskAdd day={day} addMyTask={addMyTask} trip={props.trip} user={props.user}  />
+
+      <TaskAdd day={day} addMyTask={addMyTask} trip={props.trip} user={props.user} />
     </div>
-    
+
   );
 }
